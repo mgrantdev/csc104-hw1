4bcec86e7c3a155d6d49f88509dd9cbdc1ba4f66
Matthew Grant <mgrant.xi@gmail.com>
2022-09-07 17:40:04 -0700

    added ullist files

--- ulliststr.cpp
+++ ulliststr.cpp
@@ -1,67 +1,129 @@
-#include <cstddef>
-#include <stdexcept>
 #include "ulliststr.h"
 
-ULListStr::ULListStr()
-{
-  head_ = NULL;
-  tail_ = NULL;
-  size_ = 0;
+#include <cstddef>
+#include <iostream>
+#include <stdexcept>
+
+ULListStr::ULListStr() {
+    head_ = NULL;
+    tail_ = NULL;
+    size_ = 0;
 }
 
-ULListStr::~ULListStr()
-{
-  clear();
+ULListStr::~ULListStr() { clear(); }
+
+bool ULListStr::empty() const { return size_ == 0; }
+
+size_t ULListStr::size() const { return size_; }
+
+void ULListStr::push_back(const std::string& val) {
+
+    // @summary Create new item with null reference to next
+    Item* item = new Item();
+    item->val[0] = val;
+    item->prev = tail_;
+    item->next = NULL;
+    tail_ = item;
+
+    // @condition If head is empty, make head point to new item
+    if (head_ == NULL) {
+        head_ = item;
+    }
+
+    // @condition If size will be 2,  connect the items
+    if (tail_->prev != NULL) {
+        tail_->prev->next = tail_;
+    }
+
+    size_++;
 }
 
-bool ULListStr::empty() const
-{
-  return size_ == 0;
+void ULListStr::push_front(const std::string& val) {
+
+    // @summary Create new item with null reference to previous
+    Item* item = new Item();
+    item->val[0] = val;
+    item->prev = NULL;
+    item->next = head_;
+    head_ = item;
+
+    // @summary If tail is empty, make tail point to new item
+    if (tail_ == NULL) {
+        tail_ = item;
+    }
+
+    // @condition If size will be 2,  connect the items
+    if (head_->next != NULL) {
+        head_->next->prev = head_;
+    }
+
+    size_++;
 }
 
-size_t ULListStr::size() const
-{
-  return size_;
+void ULListStr::pop_front() { 
+
+    // @summary Remove item at front of list
+    head_ = head_->next;
+    delete head_->prev;
+    head_->prev = NULL;
+    size_--;
+  
 }
 
-void push_back(const std::string& val) {
-  _tail = new Item;
+void ULListStr::pop_back() {
+
+    // @summary Remove item at end of list
+    tail_ = tail_->prev;
+    delete tail_->next;
+    tail_->next = NULL;
+    size_--;
 }
 
-void ULListStr::set(size_t loc, const std::string& val)
-{
-  std::string* ptr = getValAtLoc(loc);
-  if(ptr == NULL){
-    throw std::invalid_argument("Bad location");
-  }
-  *ptr = val;
+std::string const& ULListStr::front() const { return head_->val[0]; }
+
+std::string const& ULListStr::back() const { return tail_->val[0]; }
+
+std::string* ULListStr::getValAtLoc(size_t loc) const {
+    Item* cursorHead = head_;
+    for (size_t x = 0; x < size_; x++) {
+        if (loc == x) {
+            return cursorHead->val;
+        }
+        cursorHead = cursorHead->next;
+    }
+    return cursorHead->val;
 }
 
-std::string& ULListStr::get(size_t loc)
-{
-  std::string* ptr = getValAtLoc(loc);
-  if(ptr == NULL){
-    throw std::invalid_argument("Bad location");
-  }
-  return *ptr;
+void ULListStr::set(size_t loc, const std::string& val) {
+    std::string* ptr = getValAtLoc(loc);
+    if (ptr == NULL) {
+        throw std::invalid_argument("Bad location");
+    }
+    *ptr = val;
 }
 
-std::string const & ULListStr::get(size_t loc) const
-{
-  std::string* ptr = getValAtLoc(loc);
-  if(ptr == NULL){
-    throw std::invalid_argument("Bad location");
-  }
-  return *ptr;
+std::string& ULListStr::get(size_t loc) {
+    std::string* ptr = getValAtLoc(loc);
+    if (ptr == NULL) {
+        throw std::invalid_argument("Bad location");
+    }
+    return *ptr;
 }
 
-void ULListStr::clear()
-{
-  while(head_ != NULL){
-    Item *temp = head_->next;
-    delete head_;
-    head_ = temp;
-  }
-  tail_ = NULL;
-  size_ = 0;
+std::string const& ULListStr::get(size_t loc) const {
+    std::string* ptr = getValAtLoc(loc);
+    if (ptr == NULL) {
+        throw std::invalid_argument("Bad location");
+    }
+    return *ptr;
+}
+
+void ULListStr::clear() {
+    while (head_ != NULL) {
+        Item* temp = head_->next;
+        delete head_;
+        head_ = temp;
+    }
+    tail_ = NULL;
+    size_ = 0;
 }
--- ulliststr_test.cpp
+++ ulliststr_test.cpp
@@ -1,11 +1,20 @@
 /* Write your test code for the ULListStr in this file */
-
+#include "ulliststr.h"
 #include <iostream>
 using namespace std;
 
 
 int main(int argc, char* argv[])
 {
-
+  ULListStr dat;
+  dat.push_back("1");
+  //dat.push_front(8);
+  dat.push_front("3");
+  dat.push_back("4");
+  dat.push_front("9");
+  dat.pop_front();
+  //dat.get(0);dat.get(1);dat.get(3);
+  cout << dat.get(0) << " " << dat.get(1) << " " << dat.get(2) << endl;
+  //cout << dat.size() << end; 
   return 0;
 }
